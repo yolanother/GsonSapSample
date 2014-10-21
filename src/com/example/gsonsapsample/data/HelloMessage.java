ff71b10e5c06e02c4bde77960699b71cadad7aeb
Aaron Jackson <a1.jackson@samsung.com>
2014-10-21 17:05:22 -0500

    Initial sample implementation

---
+++ src/com/example/gsonsapsample/data/HelloMessage.java
@@ -0,0 +1,10 @@
+package com.example.gsonsapsample.data;
+
+public class HelloMessage {
+	public long timestamp;
+	public String message;
+
+	public HelloMessage(String message) {
+		this.message = message;
+	}
+}
